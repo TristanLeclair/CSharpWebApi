19a401d584f32c940f708e30e7e6585923cd7148
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2023-02-15 16:36:46 -0500

    Add God Repo and controller

---
+++ WebApplication1/Data/IGodRepo.cs
@@ -0,0 +1,18 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Data;
+
+public interface IGodRepo
+{
+    /// <summary>
+    /// Returns an IEnumerable of all the gods
+    /// </summary>
+    /// <returns>IEnumerable of all gods</returns>
+    IEnumerable<God> GetGods();
+    /// <summary>
+    /// Returns the god with the passed in name
+    /// </summary>
+    /// <param name="name">Name of the god to get</param>
+    /// <returns>God or null if no god with that name exists</returns>
+    God? GetGodByName(string name);
+}
---
+++ WebApplication1/Data/MockGodRepo.cs
@@ -0,0 +1,26 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Data;
+
+public class MockGodRepo : IGodRepo
+{
+    private readonly IEnumerable<God> _gods = new[]
+    {
+        new God("Yi the Creator God", Enumerable.Empty<Domain>(), Alignment.TrueNeutral,
+            new[] { "The First", "One", "Yi" }),
+        new God("Udar Archgod of Earth", new[] { Domain.Forge, Domain.Life }, Alignment.LawfulGood,
+            new[] { "The StoneGod", "The Second" }),
+        new God("Aeros Archgod of the Storm and Sea", new[] { Domain.Tempest }, Alignment.ChaoticGood,
+            new[] { "The Everwind", "Master of the Tides" })
+    };
+
+    public IEnumerable<God> GetGods()
+    {
+        return _gods;
+    }
+
+    public God? GetGodByName(string name)
+    {
+        return _gods.FirstOrDefault(god => god != null && god.Name.Equals(name), defaultValue: null);
+    }
+}
