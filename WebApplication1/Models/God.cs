d8f22db9cfb66f98e6a7a4c863f7f101751af9dd
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2023-02-15 18:08:27 -0500

    Update God to readonly record class

--- WebApplication1/Models/God.cs
+++ WebApplication1/Models/God.cs
@@ -1,17 +1,4 @@
 ﻿namespace WebApplication1.Models;
 
-public class God
-{
-    public string Name { get; private set; }
-    public IEnumerable<Domain> Domains { get; private set; }
-    public Alignment Alignment { get; private set; }
-    public IEnumerable<string> Titles { get; private set; }
-
-    public God(string name, IEnumerable<Domain> domains, Alignment alignment, IEnumerable<string> titles)
-    {
-        Name = name;
-        Domains = domains;
-        Alignment = alignment;
-        Titles = titles;
-    }
-}
+public readonly record struct God(string Name, IEnumerable<Domain> Domains,
+    Alignment Alignment, IEnumerable<string> Titles);
