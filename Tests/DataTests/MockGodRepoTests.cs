480697c73e1b8674eb7ce5c0408e202a7b89d43d
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2023-02-15 21:21:26 -0500

    Add tests for MockGodRepo

--- Tests/DataTests/MockGodRepoTests.cs
+++ Tests/DataTests/MockGodRepoTests.cs
@@ -1,6 +1,47 @@
-﻿namespace Tests.DataTests;
+﻿using WebApplication1.Data;
+
+namespace Tests.DataTests;
 
 public class MockGodRepoTests
 {
-    
+    private readonly MockGodRepo _godRepo = new MockGodRepo();
+
+    [Fact]
+    public void GetGods_ReturnsList_Always()
+    {
+        // Arrange
+
+        // Act
+        var gods = _godRepo.GetGods();
+
+        //Assert
+        Assert.NotNull(gods);
+    }
+
+    [Fact]
+    public void GetGodByName_ReturnsGod_WhenGodExists()
+    {
+        // Arrange
+        const string godName = "Yi the Creator God";
+
+        // Act
+        var god = _godRepo.GetGodByName(godName);
+
+        //Assert   
+        Assert.NotNull(god);
+        Assert.Equal(godName, god.Name);
+    }
+
+    [Fact]
+    public void GetGodByName_ReturnsNull_WhenGodDoesNotExist()
+    {
+        // Arrange
+        const string godName = "Yi the Non Creator God";
+
+        // Act
+        var god = _godRepo.GetGodByName(godName);
+
+        //Assert
+        Assert.Null(god);
+    }
 }
